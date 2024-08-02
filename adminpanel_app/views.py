7a64aac06dabf1f0c45b01b8d9d8f538c890afbb
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-02 17:30:08 +0530

    Added dynamic total visits

--- adminpanel_app/views.py
+++ adminpanel_app/views.py
@@ -9,6 +9,7 @@ from django.db.models.functions import TruncHour
 
 def index(request):
     today=timezone.now().date()
+    total=Visits.objects.filter(unique_link=request.session['unique_link']).count()
     visits_today = Visits.objects.filter(timestamp__date=today).annotate(hour=TruncHour('timestamp')).values('hour').annotate(count=Count('id')).order_by('hour')
 
     hours = [f"{hour:02}:00" for hour in range(24)]
@@ -19,6 +20,6 @@ def index(request):
     context={
         'total_visits_day': sum(counts),
         'visits_per_hour': counts,
-        'total_visits': 1000
+        'total_visits': total
     }
     return render(request,'dashboard.html',context)
