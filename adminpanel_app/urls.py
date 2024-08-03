3b94d3dd01de77c96fb1b29cc0402c9a28f6c78f
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-03 13:32:24 +0530

    Added weekly stats

--- adminpanel_app/urls.py
+++ adminpanel_app/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('get_week_data/', get_week_data, name='get_week_data'),
 ]
--- adminpanel_app/views.py
+++ adminpanel_app/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from redirection.models import Visits
 from django.utils import timezone
 from django.db.models import Count
 from django.db.models.functions import TruncHour
+from datetime import datetime,timedelta
+from django.http import JsonResponse
 
 
 # Create your views here.
 
 def index(request):
+
+    if 'email' not in request.session:
+        messages.error(request, 'Login Required')
+        return redirect('/auth/login/')
+    
     today=timezone.now().date()
     total=Visits.objects.filter(unique_link=request.session['unique_link']).count()
     visits_today = Visits.objects.filter(timestamp__date=today).annotate(hour=TruncHour('timestamp')).values('hour').annotate(count=Count('id')).order_by('hour')
@@ -23,3 +31,39 @@ def index(request):
         'total_visits': total
     }
     return render(request,'dashboard.html',context)
+
+def get_week_data(request):
+    week_start = request.GET.get('week_start', None)
+    if week_start:
+        week_start = timezone.make_aware(datetime.strptime(week_start, '%Y-%m-%d'))
+    else:
+        today = datetime.now().date()
+        week_start = today - timedelta(days=today.weekday())
+
+
+    week_end = week_start + timezone.timedelta(days=6)
+
+    visits = Visits.objects.filter(timestamp__range=[week_start, week_end])
+
+    visit_data = {week_start + timedelta(days=i): 0 for i in range(7)}
+
+    for visit in visits:
+        date = visit.timestamp.date()
+        if date not in visit_data:
+            visit_data[date] = 0  
+        visit_data[date] += 1
+
+    result={
+        'labels': [date.strftime('%Y-%m-%d') for date in visit_data.keys()],
+        'data': list(visit_data.values())
+    }
+
+    # fianl={}
+    # for i in data:
+    #     if i.timestamp.strftime('%Y-%m-%d') in fianl:
+    #         fianl[i.timestamp.strftime('%Y-%m-%d')]+=1
+    #     else:
+    #         fianl[i.timestamp.strftime('%Y-%m-%d')]=1
+    print(result)
+    return JsonResponse(result)
+        
