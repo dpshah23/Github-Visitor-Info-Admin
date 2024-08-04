06fc5fb89a1abf762396f84642faaee556330cf3
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-04 14:01:32 +0530

    Updated css

--- auth1/urls.py
+++ auth1/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('logout/',logout,name='logout'),
     path('validate/',validate,name='validate'),
     path('register/',signup,name='signup'),
-    
+    path('signup/',signup,name='signup'),
 ]
